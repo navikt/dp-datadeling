cea1043cf49a883f03ef94f5bd9e7771b2ce60f4
Igor Shuliakov <igor.shuliakov@nav.no>
2023-05-11 15:03:37 +0200

    Internal API

---
+++ src/main/kotlin/dp/datadeling/api/internal.kt
@@ -0,0 +1,28 @@
+package dp.datadeling.api
+
+import com.papsign.ktor.openapigen.route.path.normal.NormalOpenAPIRoute
+import com.papsign.ktor.openapigen.route.path.normal.get
+import com.papsign.ktor.openapigen.route.response.respond
+import com.papsign.ktor.openapigen.route.route
+import io.micrometer.prometheus.PrometheusMeterRegistry
+
+
+fun NormalOpenAPIRoute.internalApi(prometheusMeterRegistry: PrometheusMeterRegistry) {
+    route("/internal/liveness") {
+        get<Unit, String> {
+            respond("Alive")
+        }
+    }
+
+    route("/internal/readyness") {
+        get<Unit, String> {
+            respond("Ready")
+        }
+    }
+
+    route("/internal/prometheus") {
+        get<Unit, String> {
+            respond(prometheusMeterRegistry.scrape())
+        }
+    }
+}
