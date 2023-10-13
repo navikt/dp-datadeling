b4ef2c1b3e6b04d8d464b45d0ac6e1aeecc27b38
Stephen Ramthun <stephenramthun@gmail.com>
2023-10-13 10:03:01 +0200

    Prøv å bytte tilbake til .conf-filer

--- src/test/kotlin/no/nav/dagpenger/datadeling/LocalApp.kt
+++ src/test/kotlin/no/nav/dagpenger/datadeling/LocalApp.kt
@@ -5,11 +5,11 @@ import io.ktor.server.netty.*
 
 fun main(args: Array<String>): Unit = EngineMain.main(args)
 
-@Suppress("unused") // application.yaml refererer til modulen
-fun Application.testApplication() {
+@Suppress("unused")
+fun Application.testModule() {
     System.setProperty("DP_IVERKSETT_URL", "http://localhost:8094/api")
     System.setProperty("DP_IVERKSETT_SCOPE", "some-scope")
-    System.setProperty("DP_PROXY_URL", "http://localhost:8081/api")
+    System.setProperty("DP_PROXY_URL", "http://0.0.0.0:8092/api")
     System.setProperty("DP_PROXY_SCOPE", "some-scope")
     module()
 }
