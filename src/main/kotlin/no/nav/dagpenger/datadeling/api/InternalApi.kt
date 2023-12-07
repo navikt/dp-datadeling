9466e3f75313e6906b09d11b6cdd57e4a55973b9
Marius Eriksen <marius.eriksen@nav.no>
2023-12-07 08:48:37 +0100

    Fjern ubrukte imports

--- src/main/kotlin/no/nav/dagpenger/datadeling/api/InternalApi.kt
+++ src/main/kotlin/no/nav/dagpenger/datadeling/api/InternalApi.kt
@@ -1,14 +1,11 @@
 package no.nav.dagpenger.datadeling.api
 
-import com.papsign.ktor.openapigen.route.path.normal.NormalOpenAPIRoute
-import com.papsign.ktor.openapigen.route.path.normal.get
-import com.papsign.ktor.openapigen.route.response.respond
-import com.papsign.ktor.openapigen.route.route
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
+import io.ktor.server.application.call
+import io.ktor.server.response.respond
+import io.ktor.server.routing.Route
+import io.ktor.server.routing.get
+import io.ktor.server.routing.route
 import io.micrometer.prometheus.PrometheusMeterRegistry
-
 
 fun Route.internalApi(prometheusMeterRegistry: PrometheusMeterRegistry) {
     route("/internal/liveness") {
