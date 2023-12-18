e91a934f2627a8ea7eeeeb6b0bc4e5ef58fbeb58
Marius Eriksen <marius.eriksen@nav.no>
2023-12-18 11:57:43 +0100

    Kjør flyway migrering ved oppstart
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

--- src/main/kotlin/no/nav/dagpenger/datadeling/App.kt
+++ src/main/kotlin/no/nav/dagpenger/datadeling/App.kt
@@ -1,14 +1,19 @@
 package no.nav.dagpenger.datadeling
 
 import io.ktor.server.cio.CIO
+import io.ktor.server.cio.CIOApplicationEngine
 import io.ktor.server.engine.embeddedServer
 import mu.KotlinLogging
+import no.nav.dagpenger.datadeling.PostgresDataSourceBuilder.runMigration
 import no.nav.dagpenger.datadeling.api.datadelingApi
 
 val defaultLogger = KotlinLogging.logger {}
 
 fun main() {
+    runMigration()
     createDatadelingServer().start(wait = true)
 }
 
-fun createDatadelingServer(port: Int = 8080) = embeddedServer(port = port, module = { datadelingApi() }, factory = CIO)
+fun createDatadelingServer(port: Int = 8080): CIOApplicationEngine {
+    return embeddedServer(port = port, module = { datadelingApi() }, factory = CIO)
+}
