b8a788993d15a3c9d144960b140530fbaa44e99b
Marius Eriksen <marius.eriksen@nav.no>
2024-04-08 12:35:14 +0200

    Oppgrader til nyeste versjon av dp-kontrakter
    Co-authored-by: Giao The Cung <giaothe@gmail.com>
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 project.setProperty("mainClassName", "no.nav.dagpenger.datadeling.AppKt")
 
-val kontrakterVersion = "2.0_20231222084529_f0d8240"
+val kontrakterVersion = "3.0_20240408122747_6eff346"
 val mockOauth2Version = "2.1.3"
 val wiremockVersion = "3.0.1"
 val testcontainersVersion = "1.19.2"
