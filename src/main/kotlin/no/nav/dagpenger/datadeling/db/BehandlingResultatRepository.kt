0218234365740150af289e2ae85306283e60c9d3
Geir André Lund <geir.andre.lund@nav.no>
2025-11-05 15:19:18 +0100

    Lese json som en stream.

--- src/main/kotlin/no/nav/dagpenger/datadeling/db/BehandlingResultatRepository.kt
+++ src/main/kotlin/no/nav/dagpenger/datadeling/db/BehandlingResultatRepository.kt
@@ -127,7 +127,7 @@ class BehandlingResultatRepository {
 }
 
 fun Row.toBehandlingResultat(): BehandlingResultat {
-    val jsonData = this.string("json_data")
+    val jsonData = this.binaryStream("json_data")
     val behandlingResultatNode: JsonNode = objectMapper.readTree(jsonData)
     return BehandlingResultatV1Tolker(behandlingResultatNode)
 }
