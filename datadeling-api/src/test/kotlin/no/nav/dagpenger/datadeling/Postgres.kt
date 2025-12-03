18b5666c639b74b6f1bb72d4a06240869480517c
Geir André Lund <geir.andre.lund@nav.no>
2025-12-03 09:07:16 +0100

    Fjerner deprekert bruk av PostgreSQLContainer

--- datadeling-api/src/test/kotlin/no/nav/dagpenger/datadeling/Postgres.kt
+++ datadeling-api/src/test/kotlin/no/nav/dagpenger/datadeling/Postgres.kt
@@ -3,11 +3,11 @@ package no.nav.dagpenger.datadeling
 import com.zaxxer.hikari.HikariDataSource
 import no.nav.dagpenger.datadeling.db.PostgresDataSourceBuilder
 import org.flywaydb.core.internal.configuration.ConfigUtils
-import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.postgresql.PostgreSQLContainer
 
 internal object Postgres {
     val instance by lazy {
-        PostgreSQLContainer<Nothing>("postgres:15").apply {
+        PostgreSQLContainer("postgres:15").apply {
             start()
         }
     }
